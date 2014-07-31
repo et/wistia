3429dcab6d7fcfb5476e79a16dc1dec7f7f129ee
Eric Thomas <eric.thomas@salesforce.com>
2014-07-30 18:24:17 -0600

    use js to inject playlist

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,18 @@
-window.wistiaEmbeds.onFind(function(videoOrPlaylist) {
-  videoOrPlaylist.bind("play", function() {
-    console.log(videoOrPlaylist.currentVideo());
-    return this.unbind;
-  });
+var wistiaPlaylist = Wistia.playlist("4xledpdgdp", {
+  version: "v1",
+  theme: "tab",
+  videoOptions: {
+    volumeControl: true,
+    autoPlay: true,
+    videoWidth: "640",
+    videoHeight: "360"
+  },
+  media_0_0: {
+    autoPlay: false,
+    controlsVisibleOnLoad: false
+  }
+});
+
+wistiaPlaylist.bind('play', function() {
+  console.log(wistiaPlaylist.currentVideo());
 });
